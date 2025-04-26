ef7bf60b5a4f129b84f9a2c1ba1d6a7245da9bf7
Etienne Naude <eti@naude.dev>
2025-04-26 19:26:06 +0100

    readme

--- src/slic3r/GUI/ICRSConfig.hpp
+++ src/slic3r/GUI/ICRSConfig.hpp
@@ -1,9 +1,9 @@
 #ifndef ICRS_CONFIG_HPP
 #define ICRS_CONFIG_HPP
 
-#define ICRS_ENABLE_PRINT_ENDPOINT  "http://127.0.0.1/api/access/getPrintWindow"
-#define ICRS_PRINT_SUBMITTED        "http://127.0.0.1/api/access/postPrintTime"
-#define ICRS_TIME_CHECK             "http://127.0.0.1/api/access/checkPrintTime"
-#define ICRS_CAN_USE_ALL            "http://127.0.0.1/api/access/canUsePrinter"
+#define ICRS_ENABLE_PRINT_ENDPOINT  "http://192.168.1.113/api/access/getPrintWindow"
+#define ICRS_PRINT_SUBMITTED        "http://192.168.1.113/api/access/postPrintTime"
+#define ICRS_TIME_CHECK             "http://192.168.1.113/api/access/checkPrintTime"
+#define ICRS_CAN_USE_ALL            "http://192.168.1.113/api/access/canUsePrinter"
 
 #endif
